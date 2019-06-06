2a6e4b8d67900756f144d18d4ab3cc1bfbdcd4c0
Oren Cohen <orencodes@gmail.com>
2019-06-06 06:07:10 +0300

    editing react-components register & login

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,10 +1,129 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
-class Register extends Component {
+export interface IRegisterState {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    password2: string
+    errors: Record<any, null>
+}
+
+class Register extends Component <IRegisterState> {
+
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        password2: '',
+        errors: {
+            firstName: null,
+            lastName: null,
+            email: null,
+            password: null,
+            password2: null
+        }
+    };
+
+
+    onChange = (event: any) => {
+        this.setState({[event.target.name]: event.target.value})
+    };
+
+    onSubmit = (event: any) => {
+        event.preventDefault();
+
+        const newUser = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2,
+        };
+
+        axios.post('/api/users/register', newUser)
+            .then(res => console.log(res.data))
+            .catch(err => this.setState({errors: err.response.data}))
+    };
+
     render() {
+        const {errors} = this.state;
+
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Sign Up</h1>
+                            <p className="lead text-center">Create your Vacation Master account</p>
+                            <form noValidate onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input type="text"
+                                           className={classnames("form-control form-control-lg", {
+                                               'is-invalid': errors.firstName
+                                           })}
+                                           placeholder="First Name"
+                                           name="firstName"
+                                           value={this.state.firstName}
+                                           onChange={this.onChange}/>
+                                    {errors.firstName && (<div className="invalid-feedback">{errors.firstName}</div>)}
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        className={classnames("form-control form-control-lg", {
+                                            'is-invalid': errors.lastName
+                                        })}
+                                        placeholder="Last Name"
+                                        name="lastName"
+                                        value={this.state.lastName}
+                                        onChange={this.onChange}/>
+                                    {errors.lastName && (<div className="invalid-feedback">{errors.lastName}</div>)}
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className={classnames("form-control form-control-lg", {
+                                            'is-invalid': errors.email
+                                        })}
+                                        placeholder="Email Address"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}/>
+                                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className={classnames("form-control form-control-lg", {
+                                            'is-invalid': errors.password
+                                        })}
+                                        placeholder="Password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}/>
+                                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className={classnames("form-control form-control-lg", {
+                                            'is-invalid': errors.password2
+                                        })}
+                                        placeholder="Confirm Password"
+                                        name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange}/>
+                                    {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
+                                </div>
+                                <input type="submit" className="btn btn-info btn-block mt-4"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
