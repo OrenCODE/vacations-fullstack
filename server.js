823b61c07c2f30ec438feaae60fc501029302044
Oren Cohen <orencodes@gmail.com>
2019-07-13 20:42:26 +0300

    Prepered for deploy

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.use('/api/users', users);
 app.use('/api/vacations', vacations);
 
 // Server static assets if in production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
