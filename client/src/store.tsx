5182d55272c2f5ab7d949c37d53080d3f09606a3
Oren Cohen <orencodes@gmail.com>
2019-07-14 17:53:08 +0300

    initial commit

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -9,8 +9,8 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    compose(applyMiddleware(...middleware),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    compose(applyMiddleware(...middleware)
+        // (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
